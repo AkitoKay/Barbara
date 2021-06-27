aad1e5ec98f33c7d035f91bdefc5a3704484cb63
ErikOrtmann <saxonaut@gmx.de>
2021-06-27 23:06:03 +0200

    Changed GUI Structure

---
+++ Code/GUI_media.py
@@ -0,0 +1,26 @@
+import tkinter as tk
+from tkinter import ttk
+
+class Window(ttk.Frame):
+    def __init__(self, root):
+        ttk.Frame.__init__(self, root)
+        self._pack = ttk.Frame.pack
+
+        #instance your objects here
+        self.label = ttk.Label(self, text='Insert code for media page here')        
+
+
+
+        
+    def pack(self, **kwargs):
+        #call .pack() for instanced objects here
+        self._pack(self, kwargs)
+        self.label.pack()
+
+
+        
+if __name__=='__main__':
+    root = tk.Tk()
+    label = Window(root)
+    label.pack()
+    root.mainloop()
---
+++ Code/_ReadMe.txt
@@ -0,0 +1,8 @@
+Versucht mal die GUI_main...
+
+Problem bei Startseite mit Button ist das Mensch nicht einfach zwischen 'lent' und 'media' wechseln
+kann. Zwar können mehrere Fenster geöffnet und hin und hergeschoben werden, ist aber ... bähh.
+
+Auserdem lief es vorher auf Spagetticode hinaus. 
+Jetzt können die Fenster separat editiert werden und der Code ist auch 'geschachtelt'.
+
