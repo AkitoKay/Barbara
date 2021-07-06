4f802febb18ac7ba04ff21f16d071ed271640383
Simon Streit <simon@netpanic.org>
2021-07-06 12:57:33 +0200

    Extend database connector.
    	modified:   Code/db_connector.py

--- Code/db_connector.py
+++ Code/db_connector.py
@@ -1,25 +1,48 @@
 import mariadb
-#https://mariadb.com/de/resources/blog/how-to-connect-python-programs-to-mariadb/
+import configparser
+
+
+class config:
+    def __init__(self):
+        self.config = configparser.ConfigParser()
+        self.config.read("barbara.conf")
+        self.user = config["server"]["user"]
+        self.password = config["server"]["password"]
+        self.host = config["server"]["host"]
+        self.port = config["server"]["port"]
+        self.database = config["server"]["database"]
+
+    # def test(self):
+    #     for i in ["user", "password", "host", "port", "database"]:
+    #         if not config["server"][i]:
+    #             # print("% not set", i)
+    #             print("Config item is not set:", i)
+
 
 class DB:
-    _user = '#TODO'
-    _password = '#TODO'
-    _host = '#TODO'
-    _port = '#TODO'
-    _database = '#TODO'
+    _user = config.config.user
+    _password = config.config.password
+    _host = config.config.host
+    _port = config.config.port
+    _database = config.config.database
 
     @classmethod
     def request(cls, statement, values):
         try:
-            with mariadb.connect(cls._user,
-                                 cls._password,
-                                 cls._host, cls._port,
-                                 cls._database) as conn, conn.cursor() as cursor:
+            with mariadb.connect(
+                    cls._user,
+                    cls._password,
+                    cls._host, cls._port,
+                    cls._database
+            ) as conn, conn.cursor() as cursor:
                 cursor.execute(statement, values)
                 return cursor
         except mariadb.Error as e:
             print(f"Error: {e}")
 
+
 if __name__ == '__main__':
     DB.request('Hallo Welt!', (1,))
 
+
+#https://mariadb.com/de/resources/blog/how-to-connect-python-programs-to-mariadb/
