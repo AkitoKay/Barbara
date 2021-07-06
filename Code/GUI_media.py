5196bc7c6da757f2a82627174b755a8b635ac877
AkitoKay <86408311+AkitoKay@users.noreply.github.com>
2021-07-06 15:03:43 +0200

    Finished GUI and tested DB-Connection

--- Code/GUI_media.py
+++ Code/GUI_media.py
@@ -1,6 +1,8 @@
 import tkinter as tk
 from functools import partial
 from tkinter import ttk
+import Showbox as Sb
+import mysql.connector
 
 
 class Window(ttk.Frame):
@@ -11,7 +13,7 @@ class Window(ttk.Frame):
 
         #instance your objects here
         #self.label = ttk.Label(self, text='Insert code for media page here')
-        self.style = ttk.Style().configure('Frame1.TFrame', background='red')   #Frame Debug Tool
+        #self.style = ttk.Style().configure('Frame1.TFrame', background='red')   #Frame Debug Tool
         self.rowconfigure(0, weight=1)
         self.rowconfigure(1, weight=1)
         self.columnconfigure(0, weight=1)
@@ -19,7 +21,7 @@ class Window(ttk.Frame):
         self.cbvar = [tk.StringVar(), tk.StringVar(), tk.StringVar(), tk.StringVar()]
         self.topframe1 = ttk.Frame(self, padding=5)
         self.topframe2 = ttk.Frame(self, padding=5)
-        self.centerframe1 = ttk.Frame(self, padding=5, style='Frame1.TFrame')
+        self.centerframe1 = ttk.Frame(self, padding=5)
         self.checkbox1 = ttk.Checkbutton(self.topframe1,
                                          text='Music', variable=self.cbvar[0], onvalue='music', offvalue='')
         self.checkbox2 = ttk.Checkbutton(self.topframe1,
@@ -31,32 +33,46 @@ class Window(ttk.Frame):
         self.dropdown = ttk.Combobox(self.topframe2,
                                      values=('Author', 'Title', 'Publisher', 'Year', 'Location'), state='readonly')
         self.entry = ttk.Entry(self.topframe2)
-        self.search = ttk.Button(self.topframe2, text='Search')
-        self.showbox = tk.Label(self.centerframe1, text='Hier könnte ihre Showbox stehen!', height=28)
-        self.debugg = ttk.Button(self.centerframe1, text='test', command=self.debugger)
+        self.search = ttk.Button(self.topframe2, text='Search',
+                                 command=lambda:self.showbox.fill_window(self.get_data(self.cbvar)))
+        #self.showbox = tk.Label(self.centerframe1, text='Hier könnte ihre Showbox stehen!', height=28)
+        self.showbox = Sb.ShowBox(self.centerframe1)
+        #self.debugg = ttk.Button(self.centerframe1, text='test', command=self.debugger)
 
     def pack(self, **kwargs):
 
         #call .pack() for instanced objects here
         self._pack(self, **kwargs)
-        self.topframe1.grid(row=0, column=0, sticky='NESW')
-        self.topframe2.grid(row=0, column=1, sticky='NESW')
+        self.topframe1.grid(row=0, column=0, sticky='NESW', padx=60, pady=5)
+        self.topframe2.grid(row=0, column=1, sticky='NESW', padx=57, pady=5)
         self.centerframe1.grid(row=1, column=0, columnspan=2, sticky='NESW')
         self.checkbox1.pack(side='top', anchor='w')
         self.checkbox2.pack(side='top', anchor='w')
         self.checkbox3.pack(side='top', anchor='w')
         self.checkbox4.pack(side='top', anchor='w')
-        self.dropdown.pack(side='top', anchor='e', fill='x', pady=3, padx=1)
-        self.entry.pack(side='top', anchor='e', fill='x', pady=3, padx=1)
-        self.search.pack(side='top', anchor='e', fill='x', pady=3)
-        self.showbox.pack(side='top', anchor='center', fill='both', pady=5)
-        self.debugg.pack(side='bottom')
+        self.dropdown.pack(side='top', anchor='e', fill='x', pady=3, padx=5)
+        self.entry.pack(side='top', anchor='e', fill='x', pady=3, padx=5)
+        self.search.pack(side='top', anchor='e', fill='x', pady=3, padx=5)
+        self.showbox.pack(side='top', anchor='center', pady=5)
+        #self.debugg.pack(side='bottom')
 
     def debugger(self):
         for i in self.cbvar:
             if i.get() != '':
                 print(i.get())
         #print(self.entry1.get(), self.entry2.get(), self.entry3.get())
+
+        """def get_data(self, typo):
+        db = mysql.connector.connect(user='barbara', password='', host='192.168.8.154', database='barbara')
+        cursor = db.cursor()
+        query = ('SELECT id, titel, author, publisher, published FROM literature WHERE titel LIKE %s ORDER BY published')
+        table = (typo[0].get(), typo[1].get(), typo[2].get(), typo[3].get())
+        print(table)
+        cursor.execute(query, ('Harry%',))
+        result = cursor.fetchall()
+        cursor.close()
+        db.close()
+        return result"""
 
 
 
